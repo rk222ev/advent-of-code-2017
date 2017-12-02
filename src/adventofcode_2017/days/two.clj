c93cc02a98faf94932c7bc853d09a016b15cc259
Robin Karlsson <robinpkarlsson@gmail.com>
2017-12-02 22:14:20 +0100

    Refactor day two

--- src/adventofcode_2017/days/two.clj
+++ src/adventofcode_2017/days/two.clj
@@ -4,8 +4,25 @@
    [adventofcode-2017.utils :as utils]))
 
 
+(defn- find-product-of-first-mod-0
+  [x xs]
+  (when xs
+    (if-let [found-value (loop [n x ns xs]
+                           (let [f (first ns)]
+                             (cond
+                               (empty? ns) nil
+                               (= 0 (mod n f)) (/ n f)
+                               (= 0 (mod f n)) (/ f n)
+                               :default (recur n (rest ns)))))]
+      found-value
+      (find-product-of-first-mod-0 (first xs) (rest xs)))))
+
+
 (defn- split-each-on-tab [xs] (vec (map (fn [x] (str/split x #"\t")) xs)))
-(defn- ->ints [xs] (map (fn [x] (utils/str->int-list x)) xs))
+(defn- ->ints [xs] (map utils/str->int-list xs))
+(defn- checksum-part-1 [xs] (- (apply max xs) (apply min xs)))
+(defn- checksum-part-2 [xs] (find-product-of-first-mod-0 (first xs) (rest xs)))
+
 
 
 (defn run
@@ -14,27 +31,6 @@
    list-of-rows
    (map checksum)
    (apply +)))
-
-
-(defn per-value
-  [n xs]
-  (cond
-    (empty? xs) nil
-    (= 0 (mod n (first xs))) (/ n (first xs))
-    (= 0 (mod (first xs) n)) (/ (first xs) n)
-    :default (per-value n (rest xs))))
-
-
-(defn get-first-evenly-div
-  [n xs]
-  (if (empty? xs)
-    nil
-    (let [v (per-value n xs)]
-      (if v v (get-first-evenly-div (first xs) (rest xs))))))
-
-
-(defn checksum-part-1 [xs] (- (apply max xs) (apply min xs)))
-(defn checksum-part-2 [xs] (get-first-evenly-div (first xs) (rest xs)))
 
 
 (defn main
